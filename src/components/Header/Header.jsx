397018212339cbffd4629bde16407425a294a762
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-03-09 15:56:20 -0800

    header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import { MdLocationOn } from "react-icons/md";
+import { HiCalendar, HiSearch } from "react-icons/hi";
+
+const Header = () => {
+  const [destination, setDestination] = useState("");
+
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem">
+          <MdLocationOn className="headerIcon locationIcon" />
+          <input
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            type="text"
+            placeholder="where to go ?"
+            className="headerSearchInput"
+            name="destination"
+            id="destination"
+          />
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <HiCalendar className="headerIcon dateIcon" />
+          <div className="dateDropDown ">03/15/2024</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <div id="optionDropDown">1 adult &bull; 2 children &bull; 1 room</div>
+          <span className="seperator"></span>
+        </div>
+        <div className="headerSearchItem">
+          <button className="headerSearchBtn">
+            <HiSearch className="headerIcon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
