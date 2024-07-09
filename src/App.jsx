11aa83f782cf0e4f811b5ec3d0937c81d5522d67
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-07-08 19:17:21 -0700

    useFetch custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import { Toaster } from "react-hot-toast";
+
 import Header from "./components/Header/Header";
+
+import "./App.css";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Header />
     </div>
   );
---
+++ src/components/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+export default function useFetch(url, query = "") {
+  const [data, setData] = useState([]);
+  consr[(isLoading, setIsLoading)] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}?${query}`);
+        setData(data);
+      } catch (error) {
+        setData([]);
+        toast.error(error?.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url, query]);
+
+  return { isLoading, Data };
+}
