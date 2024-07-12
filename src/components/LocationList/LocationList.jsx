fe06c7044d11b1a716ecebbc272774a35b631984
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-07-11 22:32:11 -0700

    img url

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -13,7 +13,7 @@ const LocationList = () => {
         {data.map((item) => {
           return (
             <div className="locationItem" key={item.id}>
-              <img src={item.picture_url.url} alt={item.name} />
+              <img src={item.thumbnail_url} alt={item.name} />
               <div className="locationItemDesc">
                 <p className="location">{item.smart_location}</p>
                 <p className="name">{item.name}</p>
